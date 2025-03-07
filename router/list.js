ee53e0cc42c4627f1166228f28d4cd8edf1b7b4e
Shoaib-json <stunnerop55@gmail.com>
2025-03-07 21:29:12 +0530

    .

--- router/list.js
+++ router/list.js
@@ -1,5 +1,8 @@
 const express  = require('express');
 const router = express.Router();
+const User = require('../models/user');
+
+const { check} = require('../utils/middleware')
 
 router.get("/", (req,res)=>{
     res.render("./listing/home.ejs");
@@ -7,6 +10,12 @@ router.get("/", (req,res)=>{
 
 router.get("/services" ,(req,res) =>{
     res.render("./listing/service.ejs")
+});
+
+router.get('/admin' , check , async(req,res)=>{
+    let q = await User.findById(req.user.id);
+    console.log(q);
+    res.send("json")
 })
 
 
